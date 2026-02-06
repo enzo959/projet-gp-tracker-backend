cb8d578696b4a52265c60ca9ca6ddf3b3578d41b
Enzo Courvalet <courvaletenzo@gmail.com>
2026-02-06 12:31:57 +0100

    fix bug

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -4,9 +4,16 @@ import (
 	"log"
 
 	"github.com/enzo959/projet_gp_tracker_backend/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// Connexion à la DB
 	if err := database.Connect(); err != nil {
 		log.Fatal(err)
