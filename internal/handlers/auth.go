60708ce262634ba3e7e58dd841a44266a3f89842
Enzo Courvalet <courvaletenzo@gmail.com>
2026-02-09 12:26:23 +0100

    RIP

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,137 +1,137 @@
 package handlers
 
 import (
-    "context"
-    "encoding/json"
-    "net/http"
-    "os"
-    "time"
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/enzo959/projet_gp_tracker_backend/internal/database"
-    "github.com/golang-jwt/jwt/v5"
-    "github.com/jackc/pgconn"  //détecter les erreurs de duplication
-    "golang.org/x/crypto/bcrypt"
+	"github.com/enzo959/projet_gp_tracker_backend/internal/database"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jackc/pgconn" //détecter les erreurs de duplication
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterRequest struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type RegisterResponse struct {
-    ID    int    `json:"id"`
-    Email string `json:"email"`
+	ID    int    `json:"id"`
+	Email string `json:"email"`
 }
 
 type LoginRequest struct {
-    Email    string `json:"email"`
-    Password string `json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type LoginResponse struct {
-    Token string `json:"token"`
+	Token string `json:"token"`
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
-    var req RegisterRequest
+	var req RegisterRequest
 
-    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-        http.Error(w, "invalid json", http.StatusBadRequest)
-        return
-    }
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
 
-    if req.Email == "" || req.Password == "" {
-        http.Error(w, "email and password required", http.StatusBadRequest)
-        return
-    }
+	if req.Email == "" || req.Password == "" {
+		http.Error(w, "email and password required", http.StatusBadRequest)
+		return
+	}
 
-    hash, err := bcrypt.GenerateFromPassword(
-        []byte(req.Password),
-        bcrypt.DefaultCost,
-    )
-    if err != nil {
-        http.Error(w, "error hashing password", http.StatusInternalServerError)
-        return
-    }
+	hash, err := bcrypt.GenerateFromPassword(
+		[]byte(req.Password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		http.Error(w, "error hashing password", http.StatusInternalServerError)
+		return
+	}
 
-    var userID int
-    err = database.DB.QueryRow(
-        context.Background(),
-        `INSERT INTO users (email, password_hash, role)
+	var userID int
+	err = database.DB.QueryRow(
+		context.Background(),
+		`INSERT INTO users (email, password_hash, role)
          VALUES ($1, $2, 'user')
          RETURNING id`,
-        req.Email,
-        string(hash),
-    ).Scan(&userID)
-        // gérer les erreurs de duplication d'email
-    if err != nil {
-        if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
-            http.Error(w, "email already exists", http.StatusConflict)
-            return
-        }
-        http.Error(w, "internal server error", http.StatusInternalServerError)  // pour les autres erreurs
-        return
-    }
+		req.Email,
+		string(hash),
+	).Scan(&userID)
+	// gérer les erreurs de duplication d'email
+	if err != nil {
+		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
+			http.Error(w, "email already exists", http.StatusConflict)
+			return
+		}
+		http.Error(w, "internal server error", http.StatusInternalServerError) // pour les autres erreurs
+		return
+	}
 
-    resp := RegisterResponse{
-        ID:    userID,
-        Email: req.Email,
-    }
+	resp := RegisterResponse{
+		ID:    userID,
+		Email: req.Email,
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(resp)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(resp)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-    var req LoginRequest
+	var req LoginRequest
 
-    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-        http.Error(w, "invalid JSON", http.StatusBadRequest)
-        return
-    }
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
 
-    if req.Email == "" || req.Password == "" {
-        http.Error(w, "email and password requi", http.StatusBadRequest)
-        return
-    }
+	if req.Email == "" || req.Password == "" {
+		http.Error(w, "email and password requi", http.StatusBadRequest)
+		return
+	}
 
-    var id int
-    var hash, role string
-    err := database.DB.QueryRow(
-        context.Background(),
-        `SELECT id, password_hash, role FROM users WHERE email=$1`,
-        req.Email,
-    ).Scan(&id, &hash, &role)
+	var id int
+	var hash, role string
+	err := database.DB.QueryRow(
+		context.Background(),
+		`SELECT id, password_hash, role FROM users WHERE email=$1`,
+		req.Email,
+	).Scan(&id, &hash, &role)
 
-    if err != nil {
-        http.Error(w, "user invalide", http.StatusUnauthorized)
-        return
-    }
-    if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(req.Password)); err != nil {
-        http.Error(w, "imdp invalide", http.StatusUnauthorized)
-        return
-    }
-    secret := os.Getenv("JWT_SECRET")
-    if secret == "" {
-        http.Error(w, "JWT pas secret", http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, "user invalide", http.StatusUnauthorized)
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(req.Password)); err != nil {
+		http.Error(w, "imdp invalide", http.StatusUnauthorized)
+		return
+	}
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		http.Error(w, "JWT pas secret", http.StatusInternalServerError)
+		return
+	}
 
-    claims := jwt.MapClaims{
-        "user_id": id,
-        "role":    role,
-        "exp":     time.Now().Add(2 * time.Hour).Unix(),
-    }
+	claims := jwt.MapClaims{
+		"user_id": id,
+		"role":    role,
+		"exp":     time.Now().Add(2 * time.Hour).Unix(),
+	}
 
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    // signer le token avec la clé secrète
-    tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-    if err != nil {
-        http.Error(w, "vous ne pouvez pas créer de token", http.StatusInternalServerError)
-        return
-    }
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// signer le token avec la clé secrète
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		http.Error(w, "vous ne pouvez pas créer de token", http.StatusInternalServerError)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(LoginResponse{Token: tokenString})
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(LoginResponse{Token: tokenString})
 }
