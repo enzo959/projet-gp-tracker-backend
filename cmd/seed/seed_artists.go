ad7ceb7d1c601201d05d279c6219c3ff8a4e3c8a
Enzo Courvalet <courvaletenzo@gmail.com>
2026-02-10 09:29:10 +0100

    add url picture

--- cmd/seed/seed_artists.go
+++ cmd/seed/seed_artists.go
@@ -9,21 +9,46 @@ import (
 
 func seedArtists() error {
 
-	artists := []string{
-		"Daft Punk",
-		"Coldplay",
-		"Radiohead",
-		"Adele",
-		"Imagine Dragons",
+	artists := []struct {
+		Name     string
+		ImageURL string
+	}{
+		{
+			Name:     "Daft Punk",
+			ImageURL: "https://upload.wikimedia.org/wikipedia/commons/8/83/Daft_Punk_in_2013_2.jpg",
+		},
+		{
+			Name:     "Coldplay",
+			ImageURL: "https://upload.wikimedia.org/wikipedia/commons/c/cc/ColdplayWembley120925_%28cropped%29.jpg",
+		},
+		{
+			Name:     "Radiohead",
+			ImageURL: "https://upload.wikimedia.org/wikipedia/commons/a/a1/RadioheadO2211125_composite.jpg",
+		},
+		{
+			Name:     "Adele",
+			ImageURL: "https://upload.wikimedia.org/wikipedia/commons/6/68/Adele.jpg",
+		},
+		{
+			Name:     "Imagine Dragons",
+			ImageURL: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Imagine_Dragons%2C_Roundhouse%2C_London_%2835390234536%29_%28cropped%29.jpg",
+		},
+		{
+			Name:     "Gims",
+			ImageURL: "https://upload.wikimedia.org/wikipedia/commons/6/67/NDLE2025Gims_1.jpg",
+		},
 	}
 
 	ctx := context.Background()
 
-	for _, name := range artists {
+	for _, artist := range artists {
 		_, err := database.DB.Exec(
 			ctx,
-			`INSERT INTO artists (name) VALUES ($1) ON CONFLICT DO NOTHING`,
-			name,
+			`INSERT INTO artists (name, image_url)
+			 VALUES ($1, $2)
+			 ON CONFLICT DO NOTHING`,
+			artist.Name,
+			artist.ImageURL,
 		)
 		if err != nil {
 			log.Fatal(err)
--- migrations/000001_create_artists_table.up.sql
+++ migrations/000001_create_artists_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     image_url TEXT,
     created_at TIMESTAMP DEFAULT now()
 );
+
